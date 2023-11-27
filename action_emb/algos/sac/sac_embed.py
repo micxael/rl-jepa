add kl loss; fix sampled representations;

action_emb/algos/sac/sac_embed.py
@@ -112,16 +112,12 @@ class SACActorCriticEmbed(BaseMLPActorCritic):
             self.steps_taken += 1
             if isinstance(action, torch.Tensor):
                 action = action.numpy()
-            return action, action_embed, None, None
+            return action, action_embed
 
         with torch.no_grad():
-            s_embed_mu = self.embedder.get_state_embedding(state, deterministic=True)
-            action_embed_mu, _ = self.pi(s_embed_mu, False, False)
-            action_mu = self.embedder.map_to_action(action_embed_mu)
-
-            s_embed_sampled = self.embedder.get_state_embedding(state, deterministic=False)
-            action_embed_sampled, _ = self.pi(s_embed_sampled, False, False)
-            action_sampled = self.embedder.map_to_action(action_embed_sampled)
+            s_embed = self.embedder.get_state_embedding(state)
+            action_embed, _ = self.pi(s_embed, False, False)
+            action = self.embedder.map_to_action(action_embed)
 
             self.steps_taken += 1
         if (
@@ -130,11 +126,9 @@ class SACActorCriticEmbed(BaseMLPActorCritic):
         ):
             self.embedder.update(buffer, logger, False)
         # For the Mujoco environments, we have a tensor as the action while the other envs are already numpy
-        if isinstance(action_mu, torch.Tensor):
-            action_mu = action_mu.numpy()
-        if isinstance(action_sampled, torch.Tensor):
-            action_sampled = action_sampled.numpy()
-        return action_mu, action_embed_mu.numpy(), action_sampled, action_embed_sampled.numpy()
+        if isinstance(action, torch.Tensor):
+            action = action.numpy()
+        return action, action_embed.numpy()
 
     def test_step(self, state):
         with torch.no_grad():
@@ -150,7 +144,7 @@ class SACActorCriticEmbed(BaseMLPActorCritic):
     def compute_loss_pi(self, experiences_dict):
         o = experiences_dict["obs"]
         with torch.no_grad():
-            o_emb = self.embedder.get_state_embedding(o, deterministic=True)
+            o_emb = self.embedder.get_state_embedding(o)
 
         pi, logp_pi = self.pi(o_emb)
         q1_pi = self.q1(o_emb, pi)
@@ -177,9 +171,9 @@ class SACActorCriticEmbed(BaseMLPActorCritic):
         )
 
         with torch.no_grad():
-            o_emb = self.embedder.get_state_embedding(o, deterministic=True)
+            o_emb = self.embedder.get_state_embedding(o)
             new_o2_sequence = torch.cat((o[:, -4 * o2.shape[1] :], o2), dim=1)
-            o2_emb = self.embedder.get_state_embedding(new_o2_sequence, deterministic=True)
+            o2_emb = self.embedder.get_state_embedding(new_o2_sequence)
 
         q1 = self.q1(o_emb, a_emb)
         q2 = self.q2(o_emb, a_emb)

action_emb/algos/sac_embed.py
@@ -148,36 +148,25 @@ def run_sac_epoch(
         obs_sequence[-1] = o
         flattened_sequence = obs_sequence.flatten()
 
-        action_mu, action_embed_mu, action_sampled, action_embed_sampled = ac.step(
+        action, action_embed = ac.step(
             torch.as_tensor(flattened_sequence, dtype=torch.float32),
             episode_num,
             buffer,
             logger,
         )
-        next_o_mu, r_mu, d_mu, _ = env.step(action_mu)
-        ep_ret += r_mu
+        next_o, r, d, _ = env.step(action)
+        ep_ret += r
         ep_len += 1
-        d = False if ep_len == cnf_train["max_ep_len"] else d_mu
+        d = False if ep_len == cnf_train["max_ep_len"] else d
         buffer.store(
             obs=flattened_sequence,
-            act=action_mu,
-            act_emb=action_embed_mu,
-            rew=r_mu,
-            next_obs=next_o_mu,
-            done=d_mu,
+            act=action,
+            act_emb=action_embed,
+            rew=r,
+            next_obs=next_o,
+            done=d,
         )
-        o = next_o_mu
-
-        if action_sampled is not None and action_embed_sampled is not None:
-            next_o_sampled, r_sampled, d_sampled, _ = env.step(action_sampled)
-            buffer.store(
-                obs=flattened_sequence,
-                act=action_sampled,
-                act_emb=action_embed_sampled,
-                rew=r_sampled,
-                next_obs=next_o_sampled,
-                done=d_sampled,
-            )
+        o = next_o
 
         timeout = ep_len == cnf_train["max_ep_len"]
         terminal = d or timeout

action_emb/embedding_modules/nn.py
@@ -7,13 +7,17 @@ class GaussianMLP(nn.Module):
     def __init__(self, inp_dim, out_dim):
         super().__init__()
 
-        self.mu_layer = nn.Linear(inp_dim, out_dim)
-        self.log_std_layer = nn.Linear(inp_dim, out_dim)
+        self.mu = nn.Linear(inp_dim, out_dim)
+        self.log_var = nn.Linear(inp_dim, out_dim)
 
-    def forward(self, obs, deterministic=False):
-        mu = self.mu_layer(obs)
-        log_std = self.log_std_layer(obs)
-        std = torch.exp(log_std)
+    def forward(self, obs, deterministic=False, return_mu_log_var=False):
+        mu = self.mu(obs)
+        log_var = self.log_var(obs)
+        std = torch.exp(0.5 * log_var)
 
         out = mu if deterministic else Normal(mu, std).rsample()
+
+        if return_mu_log_var:
+            return mu, log_var
+
         return out

action_emb/embedding_modules/state_embedding_modules/SAS_embedder.py
@@ -73,6 +73,12 @@ class SASEmbeddingModule(SAEmbeddingModule):
         loss = self.loss_fn(pred, y)
         return loss
 
+    def kl_loss(self, o):
+        with torch.no_grad():
+            mu, log_var = self.embedder.get_state_embedding_mu_logvar(o)
+        loss = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
+        return loss
+
     def update(self, buffer, logger: EpochLogger, pre_train=False):
         if self.cnf["embed"]["use_full_buffer"] or pre_train:
             experiences_dict = buffer.get_data()
@@ -107,19 +113,21 @@ class SASEmbeddingModule(SAEmbeddingModule):
             batch_size = self.cnf["embed"]["batch_size"]
             train_iters = self.cnf["embed"]["train_iters"]
         train_dataloader = du.DataLoader(data, batch_size=batch_size)
-        self.train(train_iters, train_dataloader, pre_train, logger)
+
+        kl_lambda = self.cnf["training"]["kl_lambda"]
+        self.train(train_iters, train_dataloader, kl_lambda, pre_train, logger)
 
         # This stores the updated weight tables in a separate member
         self.embedder.update_embeddings()
 
-    def train(self, train_iters, train_dataloader, pre_train, logger: EpochLogger):
+    def train(self, train_iters, train_dataloader, kl_lambda, pre_train, logger: EpochLogger):
         for _ in range(train_iters):
             self.embedder.train()
             for _, (s_x, a_x, y, a_raw) in enumerate(train_dataloader):
 
                 self.optim.zero_grad()
                 pred = self.embedder(s_x, a_x)
-                loss = self.loss(pred, y)
+                loss = self.loss(pred, y) + kl_lambda * self.kl_loss(s_x)
                 loss.backward()
                 self.optim.step()
                 if (
@@ -217,7 +225,7 @@ class SASEmbedder(nn.Module):
         self.update_embeddings()
 
     def forward(self, state_x, act_x):
-        state_emb = self.act_state(self.linear_state(state_x, deterministic=False))
+        state_emb = self.act_state(self.linear_state(state_x))
         act_emb = self.act_action(self.linear_act(act_x))
         x = torch.cat([state_emb, act_emb], dim=-1)
         x = self.activation(self.transition(x))
@@ -225,7 +233,7 @@ class SASEmbedder(nn.Module):
 
     def update_embeddings(self):
         # Note: This is only useful for indexing with discrete spaces
-        self.state_embedding = self.act_state(self.linear_state.mu_layer.weight.data)
+        self.state_embedding = self.act_state(self.linear_state.mu.weight.data)
         self.act_embedding = torch.transpose(
             self.act_action(self.linear_act.weight.data), 0, 1
         )
@@ -240,6 +248,9 @@ class SASEmbedder(nn.Module):
 
     def get_state_embedding(self, state, deterministic=False):
         return self.act_state(self.linear_state(state, deterministic)).squeeze()
+
+    def get_state_embedding_mu_logvar(self, state):
+        return self.linear_state(state, return_mu_log_var=True)
 
     def get_action_embedding(self, action):
         if (